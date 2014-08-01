fd686bf4ca9cbc4c42dc306c759caaac1421c465
HieuNguyen94 <nguyenkim94er@gmail.com>
2014-07-31 23:25:17 -0700

    Change background

--- script/state/play.js
+++ script/state/play.js
@@ -1,7 +1,7 @@
 var playState = {
 
 	create: function() {
-		this.bg = game.add.tileSprite(0, 0, 800, 480, 'bg');
+		this.bg = game.add.tileSprite(0, 0, 2365, 1536, 'bg');
 		var scale = w/this.bg.width;
 		this.bg.scale.x =  scale;
 		this.bg.scale.y =  scale;
