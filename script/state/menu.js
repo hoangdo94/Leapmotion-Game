7af7f92d5650a2bfc369c4f7576e0cb6dc6cef19
HoangDo94 <mon@fdzteam.com>
2014-08-10 17:39:17 +0700

    minor update

--- script/state/menu.js
+++ script/state/menu.js
@@ -5,7 +5,7 @@ var menuState = {
 		logo.anchor.setTo(0.5);
 		game.add.tween(logo).to({ y: h/2-80 }, 1000, Phaser.Easing.Bounce.Out).start();
 		
-		var label = game.add.text(w/2, h/2, 'Press ENTER to use Keyboard Controller\nPress SPACE to use Leap Controller', { font: '20px Arial', fill: '#fff' });
+		var label = game.add.text(w/2, h/2, 'Press ENTER to use Keyboard Controller\nPress SPACE to use Leapmotion Controller', { font: '20px Arial', fill: '#fff' });
 		label.anchor.setTo(0.5);
 		label.alpha = 0;
 		game.add.tween(label).delay(500).to({ alpha: 1}, 1000).start();
